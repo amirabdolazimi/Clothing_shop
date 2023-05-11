9ffd432229abd8d63fc0b025ad3316dcbb05a156
amirabdolazimi <amirabdolazimi.v@gmail.com>
2023-05-11 10:37:54 +0330

    footer UI bug fixed

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,7 +1,7 @@
 import { RiCopyrightLine } from "react-icons/ri";
 const Footer = () => {
   return (
-    <div className="px-4 py-8 bg-orange-500 text-white flex items-center">
+    <div className="px-4 py-8 bg-orange-500 justify-center text-white flex items-center">
       <div className="text-sm">
         تمامی حقوق این وبسایت متعلق به
         <span className="font-medium">{"( امیر عبدالعظیمی )"}</span> است
