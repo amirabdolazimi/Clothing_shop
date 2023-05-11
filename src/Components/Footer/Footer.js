ac54af3f3960b83802295a6b9730399b90b69c43
amirabdolazimi <amirabdolazimi.v@gmail.com>
2023-05-11 09:58:52 +0330

    footer font-size changed

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,9 +1,9 @@
 import { RiCopyrightLine } from "react-icons/ri";
 const Footer = () => {
   return (
-    <div className="px-12 justify-center py-8 bg-orange-500 text-white flex items-center">
+    <div className="px-4 justify-center py-8 bg-orange-500 text-white flex items-center">
       <div>
-        تمامی حقوق این وبسایت متعلق به{" "}
+        تمامی حقوق این وبسایت متعلق به
         <span className="font-semibold">{"( امیر عبدالعظیمی )"}</span> است
       </div>
       <div className="mr-0.5 mt-0.5 font-bold text-xl">
