524352fe47bf2369aed7dac2729e2fb0b8c7695f
amirabdolazimi <amirabdolazimi.v@gmail.com>
2023-05-14 19:55:34 +0330

    404  icon removed

--- src/Components/Pages/NotFoundPage/NotFoundPage.js
+++ src/Components/Pages/NotFoundPage/NotFoundPage.js
@@ -1,4 +1,3 @@
-import { TbError404 } from "react-icons/tb";
 const NotFound = () => {
   return (
     <div className="flex select-none brightness-200 justify-center font-medium items-center text-2xl py-10 text-slate-700 opacity-40 lg:text-4xl">
